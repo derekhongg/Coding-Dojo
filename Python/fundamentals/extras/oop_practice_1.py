dfc35f2be37b3b2d682587590d017d52d1ae2356
derekhongg <hongderek@gmail.com>
2022-01-06 16:36:46 -0800

    new commit

--- Python/fundamentals/extras/oop_practice_1.py
+++ Python/fundamentals/extras/oop_practice_1.py
@@ -8,47 +8,28 @@ class User:
 
     def make_deposit(self, amount):
         self.amount += amount
+        return self
     
     def make_withdrawal(self, amount):
         self.amount-= amount
+        return self
     
     def display_user_balance(self):
         print(f"User: {self.first_name} {self.last_name}, Balance: {self.amount}")
+        return self
 
-    def greeting(self):
-        print(f"Hello my name is {self.first_name} and I am {self.age} years old! I have {self.amount} in my bank account loool")
-    
     def transfer_money(self, amount, user):
         self.amount -= amount
         user.amount += amount
-        self.display_user_balance()
-        user.display_user_balance()
-
-    
+        return self
+        return user
 
 derek = User("Derek", "Hong", 24)
 peter = User("Peter", "Mach", 24)
 james = User("James", "Lee", 25)
 andre = User("Andre", "Huang", 26)
 
-derek.make_deposit(20000)
-derek.make_deposit(2350)
-derek.make_deposit(6270)
-derek.make_withdrawal(6000)
-peter.make_deposit(12389)
-peter.make_deposit(11389)
-peter.make_withdrawal(6000)
-peter.make_withdrawal(2000)
-james.make_deposit(50000)
-james.make_withdrawal(6000)
-james.make_withdrawal(5000)
-james.make_withdrawal(13847)
-andre.make_deposit(11345)
-
-
-derek.display_user_balance()
-peter.display_user_balance()
-james.display_user_balance()
-andre.display_user_balance()
-
-derek.transfer_money(1000, andre)
+derek.make_deposit(20000).make_deposit(2350).make_deposit(6270).make_withdrawal(6000).transfer_money(1000, andre).display_user_balance()
+peter.make_deposit(12389).make_deposit(11389).make_withdrawal(6000).make_withdrawal(2000).display_user_balance()
+james.make_deposit(50000).make_withdrawal(6000).make_withdrawal(5000).make_withdrawal(13847).display_user_balance()
+andre.make_deposit(11345).display_user_balance()
